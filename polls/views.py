0aa11ed869f18c3ef0cb619f1e030a2eaeabd2cb
Marco De Canal <marco.decanal@gmail.com>
2024-02-23 18:52:30 +0100

    introducing tests

--- polls/views.py
+++ polls/views.py
@@ -3,31 +3,34 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#    return render(request, "polls/index.html",context)
+
+# def index(request):
     #template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
     #output = ", ".join([q.question_text for q in latest_question_list])
     #return HttpResponse(template.render(context, request))
-    return render(request, "polls/index.html",context)
 
-def detail(request, question_id):
+#def detail(request, question_id):
     # try:
     #     question = Question.objects.get(pk=question_id)
     # except Question.DoesNotExist:
     #     raise Http404("Question does not exist")
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    #question = get_object_or_404(Question, pk=question_id)
+    #return render(request, "polls/detail.html", {"question": question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request,'polls/results.html',{"question":question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request,'polls/results.html',{"question":question})
 
 class IndexView(generic.ListView):
     template_name="polls/index.html"
@@ -35,7 +38,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
